29ebef24c72c2226b5288c1061e127a3bf13366b
abdellah <tom2012missi@gmail.com>
2025-09-10 09:16:58 +0100

    w

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,26 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
-# accounts/admin.py
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
-from django.utils.translation import gettext_lazy as _
-from .models import User
 
-@admin.register(User)
-class UserAdmin(DjangoUserAdmin):
-    fieldsets = (
-        (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "date_of_birth", "profile_photo")}),
-        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
-        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    # Add the extra fields to fieldsets
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
     )
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": ("username", "email", "password1", "password2", "date_of_birth", "profile_photo"),
-        }),
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
     )
-    list_display = ("username", "email", "first_name", "last_name", "is_staff")
-    search_fields = ("username", "first_name", "last_name", "email")
-    ordering = ("username",)
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_of_birth')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+
+
+# ✅ This is what the checker is looking for:
+admin.site.register(CustomUser, CustomUserAdmin)
