5f17e2f8d9620346a8fac127a3cbaebb5351f5de
abdellah <tom2012missi@gmail.com>
2025-09-10 09:13:04 +0100

    k

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,29 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils.translation import gettext_lazy as _
 from .managers import UserManager
 
-def user_profile_upload_to(instance, filename):
-    # e.g., users/42/profile_photo.png
-    return f"users/{instance.pk or 'new'}/profile/{filename}"
-
-class User(AbstractUser):
-    date_of_birth = models.DateField(_("date of birth"), null=True, blank=True)
-    profile_photo = models.ImageField(
-        _("profile photo"),
-        upload_to=user_profile_upload_to,
-        null=True,
-        blank=True,
-    )
+class User(AbstractUser):   # or class CustomUser(AbstractUser) if required
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
     objects = UserManager()
 
-    class Meta:
-        verbose_name = _("user")
-        verbose_name_plural = _("users")
-
     def __str__(self):
-        return self.get_username()
+        return self.username
 
 # in any app e.g., core/models.py
 from django.conf import settings
