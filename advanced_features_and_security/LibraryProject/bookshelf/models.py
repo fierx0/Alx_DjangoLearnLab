1da197701b6c58e2010029e5cfbfab62b3a6a6d0
abdellah <tom2012missi@gmail.com>
2025-09-10 09:14:53 +0100

    y

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,24 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from .managers import UserManager
 
-class User(AbstractUser):   # or class CustomUser(AbstractUser) if required
+class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
-    objects = UserManager()
-
     def __str__(self):
         return self.username
-
-# in any app e.g., core/models.py
-from django.conf import settings
-from django.db import models
-
-class Article(models.Model):
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE,
-        related_name='articles'
-    )
-    # ...
