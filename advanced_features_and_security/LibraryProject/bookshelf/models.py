1e04d8f711628fe344165a28544314cd6aa50c96
abdellah <tom2012missi@gmail.com>
2025-09-11 09:54:08 +0100

    H

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -52,7 +52,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class Article(models.Model):
+class Book(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="articles")
