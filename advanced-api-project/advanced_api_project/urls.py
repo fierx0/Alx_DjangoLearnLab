7a30c77cdedc9ef690be310c57e857258a9e7876
abdellah <tom2012missi@gmail.com>
2025-09-28 19:40:10 +0100

    Routing: add api/urls for Book generic CRUD endpoints and include in project urls

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from api.views import AuthorListCreateView, BookListCreateView, api_root
 
 urlpatterns = [
     path("", api_root, name="api-root"),
     path("admin/", admin.site.urls),
+    # existing endpoints
     path("api/authors/", AuthorListCreateView.as_view(), name="authors"),
     path("api/books/", BookListCreateView.as_view(), name="books"),
+    # NEW generic Book endpoints
+    path("api/", include("api.urls")),
 ]
