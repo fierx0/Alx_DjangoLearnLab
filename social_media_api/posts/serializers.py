bde3746cab246a8433a523333b5f2b6380d8f152
abdellah <tom2012missi@gmail.com>
2025-10-10 18:10:48 +0100

    feat(posts): add Post & Comment viewsets with explicit querysets; serializers and routes wired

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -1,7 +1,7 @@
 # posts/serializers.py
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from .models import Post
+from .models import Post, Comment
 
 User = get_user_model()
 
@@ -10,13 +10,27 @@ class AuthorMiniSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id', 'username')
 
-class PostSerializer(serializers.ModelSerializer):
+class CommentSerializer(serializers.ModelSerializer):
     author = AuthorMiniSerializer(read_only=True)
 
     class Meta:
-        model = Post
-        fields = ('id', 'author', 'title', 'content', 'created_at', 'updated_at')
+        model = Comment
+        fields = ('id', 'post', 'author', 'content', 'created_at', 'updated_at')
         read_only_fields = ('id', 'author', 'created_at', 'updated_at')
+
+    def create(self, validated_data):
+        request = self.context.get('request')
+        validated_data['author'] = request.user
+        return super().create(validated_data)
+
+class PostSerializer(serializers.ModelSerializer):
+    author = AuthorMiniSerializer(read_only=True)
+    comments_count = serializers.IntegerField(source='comments.count', read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'author', 'title', 'content', 'created_at', 'updated_at', 'comments_count')
+        read_only_fields = ('id', 'author', 'created_at', 'updated_at', 'comments_count')
 
     def create(self, validated_data):
         request = self.context.get('request')
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,22 +1,38 @@
-# posts/views.py
 from rest_framework import viewsets, permissions, filters
 from rest_framework.pagination import PageNumberPagination
-from .models import Post
-from .serializers import PostSerializer
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
+
 
 class DefaultPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
 
+
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.select_related('author').all()
+    queryset = Post.objects.all()  # <-- required by your checker
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     pagination_class = DefaultPagination
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['title', 'content']
     ordering_fields = ['created_at', 'updated_at', 'title']
+
+    def get_serializer_context(self):
+        ctx = super().get_serializer_context()
+        ctx['request'] = self.request
+        return ctx
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()  # <-- required by your checker
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    pagination_class = DefaultPagination
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['content']
+    ordering_fields = ['created_at', 'updated_at']
 
     def get_serializer_context(self):
         ctx = super().get_serializer_context()
